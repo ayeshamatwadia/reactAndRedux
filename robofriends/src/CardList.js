e6f1c65d6ceb3955c6ee1bd6278d342f2a897f8f
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-04 15:27:16 +0200

    removed the need for const and did the looping inside of the return

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
 const CardList = ({ robots }) => {
-    // for optimizations purposes give the looped components a key
-    const cardsArray = robots.map((user, i) => {
-        return <Card key={i} id={user.id} name={user.name} email={user.email}/>
-    });
-    return (
+  // for optimizations purposes give the looped components a key
+  return (
     <div>
-        {cardsArray}
+      {robots.map((user, i) => {
+        return (
+          <Card key={i} id={user.id} name={user.name} email={user.email} />
+        );
+      })}
     </div>
- )
-}
+  );
+};
 
 export default CardList;
