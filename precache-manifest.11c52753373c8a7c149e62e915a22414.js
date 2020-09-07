92f55a474ee8179bdfff0727b890cc58f59e31f1
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-07 16:30:51 +0200

    Updates

---
+++ precache-manifest.11c52753373c8a7c149e62e915a22414.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae438def8b18591e20ec1ac3e9677f45",
+    "url": "/reactAndRedux/tree/master/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/reactAndRedux/tree/master/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f14472b01e2391ed621e",
+    "url": "/reactAndRedux/tree/master/robofriends/static/css/main.b53e64fd.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/reactAndRedux/tree/master/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactAndRedux/tree/master/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14472b01e2391ed621e",
+    "url": "/reactAndRedux/tree/master/robofriends/static/js/main.70b2262a.chunk.js"
+  },
+  {
+    "revision": "46bfd24a837b929a3f93",
+    "url": "/reactAndRedux/tree/master/robofriends/static/js/runtime-main.3c25d9b5.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/reactAndRedux/tree/master/robofriends/static/media/SEGA.82e09f78.ttf"
+  }
+]);
