dea7aa7ca516d7fcfdf56563fa2557039543d3ef
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-07 16:32:30 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactAndRedux/tree/master/robofriends/precache-manifest.11c52753373c8a7c149e62e915a22414.js"
+  "/robofriends/precache-manifest.52fd682ad1fea98abbc992a06f6fedb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactAndRedux/tree/master/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
