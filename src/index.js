5dd74ed234b6ac73dd574afdfaba7165efd5f23c
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-08 20:53:34 +0200

    added redux thunk to take care of making async calls to the users API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger} from 'redux-logger';
-import { searchRobots } from './reducers' 
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers' 
 import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots});
+
 // creating the state/store
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 // we don't want to have to pass the state down as props
 // all the time so we wrap the app component in the Provider
